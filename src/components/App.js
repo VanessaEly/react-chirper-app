2ca9763b1880c594efc0c0926df232aeb614418d
Vanessa Ely <vah_ely@hotmail.com>
2019-04-02 00:06:00 -0300

    Added dashboard and appss mapStateToProps function, which avoid the render of the dashboard while we don't have our authedUser filled

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux';
 import { handleInitialData } from '../actions/shared';
+import Dashboard from './Dashboard';
 
 class App extends Component {
   componentDidMount() {
@@ -10,14 +11,31 @@ class App extends Component {
   render() {
     return (
       <div>
-        Starter Code
+        { // only load the dashboard if the loading property is true
+          this.props.loading ? null : <Dashboard></Dashboard>
+        }
       </div>
     )
   }
 }
 
+// This function is used to make sure that we won't render the UI while authedUser variable isn't filled
+function mapStateToProps ({ authedUser}) {
+  return {
+    loading: authedUser === null,
+  }
+};
+
 // Using the connect() function upgrades a component to a container.
 // Containers can read state from the store and dispatch actions.
 // in order to have access to dispatch, we need to connect our app.
 // Since we don't need anything from the state, we can leave the first invocation empty
-export default connect()(App)
+export default connect(mapStateToProps)(App)
+
+// connect([mapStateToProps], [mapDispatchToProps], [mergeProps], [options])
+
+// mapDispatchToProps - If an object is passed, each function inside it is assumed to be a Redux action creator.
+// An object with the same function names, but with every action creator wrapped into a dispatch call so they may
+// be invoked directly, will be merged into the component’s props. If a function is passed, it will be given dispatch
+// as the first parameter. It’s up to you to return an object that somehow uses dispatch to bind action creators
+// in your own way. (Tip: you may use the bindActionCreators() helper from Redux.)
---
+++ src/components/Dashboard.js
@@ -0,0 +1,37 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+
+class Dashboard extends Component {
+  render() {
+    console.log(this.props);
+    return (
+      <div>
+        <h3 className='center'>Your Timeline</h3>
+        <ul className='dashboard-list'>
+            {// looping through all tweetIds that were returned by our mapStateToProps function
+            }
+            {this.props.tweetIds.map((id) => (
+                <li key={id}>
+                    <div>ID: {id}</div>
+                </li>
+            ))}
+        </ul>
+      </div>
+    );
+  };
+};
+
+// mapStateToProps - If this argument is specified, the new component will subscribe to Redux store updates.
+// This means that any time the store is updated, mapStateToProps will be called. The results of mapStateToProps
+// must be a plain object, which will be merged into the component’s props. If you don't want to subscribe to store updates,
+// pass null or undefined in place of mapStateToProps.
+// mapStateToProps(state, [ownProps])
+function mapStateToProps ({ tweets }) {
+    // adding tweetids to the state that we are interested into
+    return {
+        // getting all tweet ids and sorting them by timestamp
+        tweetIds: Object.keys(tweets).sort((a, b) => tweets[b].timestamp - tweets[a].timestamp),
+    };
+};
+
+export default connect(mapStateToProps)(Dashboard);
